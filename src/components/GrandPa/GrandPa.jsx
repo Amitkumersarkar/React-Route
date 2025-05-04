52aff4b7dd4b11f2f2184e1cd661209deb812e3b
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-05-04 22:06:20 +0600

    added new code

--- src/components/GrandPa/GrandPa.jsx
+++ src/components/GrandPa/GrandPa.jsx
@@ -1,16 +1,23 @@
+import { createContext } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './GrandPa.css';
+
+// props drilling using context 
+export const AssetContext = createContext('Gold');
+ 
 const GrandPa = () => {
     const asset = 'diamond';
     return (
         <div className="grandpa">
             <h2>GrandPa</h2>
             <section className="flex">
-                <Dad asset={asset}></Dad>
-                <Uncle asset={asset}></Uncle>
-                <Aunty asset={asset}></Aunty>
+                <AssetContext.Provider value="Gold">
+                    <Dad asset={asset}></Dad>
+                    <Uncle asset={asset}></Uncle>
+                    <Aunty asset={asset}></Aunty>
+                </AssetContext.Provider>
             </section>
         </div>
     );
--- src/components/Wife/Wife.jsx
+++ src/components/Wife/Wife.jsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { AssetContext } from "../GrandPa/GrandPa";
 
 const Wife = ({ asset }) => {
-
+    const gift = useContext(AssetContext)
     return (
         <div>
             <h2>Wife</h2>
             <p>Has : {asset}</p>
+            <p>Also Has : {gift}</p>
         </div>
     );
 };
